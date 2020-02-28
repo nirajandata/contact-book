7f280ff27930141134fd3164c25b5223df3c9c7e
Nirajan Dhakal <53692745+nirajandata@users.noreply.github.com>
2020-02-28 18:53:13 +0545

    Add files via upload

--- contact/config.php
+++ contact/config.php
@@ -3,9 +3,15 @@
 $connec=mysqli_connect("localhost","root","","contact");
 
 if($connec){
-	//echo "s";
+	echo "<center>-------</center>";
 }
 else{
 	die("error is".mysqli_connect_error());
+}
+if (isset($_POST['save'])){
+	$name=$_POST['name'];
+	$address=$_POST['address'];
+	$email=$_POST['email'];
+	$connec->query("INSERT INTO ctable(name,email,address) VALUES('$name','$email','$address')") or die($connec->error);
 }
 ?>
--- contact/index.php
+++ contact/index.php
@@ -2,26 +2,29 @@
 <html>
 <head>
 	<title>Contact Form</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
 	<?php require_once 'config.php'; ?>
-
+<div class="row justify-content-center">
 	<form action="" method="post">
 		<div class="form-group" >
 			<label>Name</label>
-			<input type="text" name="name" placeholder="Enter your name">
+			<input type="text" class="form-control" name="name" placeholder="Enter your name">
 		 </div>
 		 <div class="form-group" >
 			<label>Email</label>
-			<input type="text" name="email" placeholder="Enter your email">
+			<input type="text" name="email" class="form-control" placeholder="Enter your email">
 		 </div>
 		  <div class="form-group" >
 			<label>Address</label>
-			<input type="text" name="address" placeholder="Enter your address">
+			<input type="text" name="address" class="form-control" placeholder="Enter your address">
 		 </div>
 		 <div class="form-group">
-		 	<button type="submit" name="save">Save</button>
+		 	<button type="submit" class="btn btn-primary" name="save">Save</button>
 		 </div>
 	</form>
+</div>
+	
 </bdoy>
 </html>
