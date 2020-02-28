a3cbfcf7598a63b1c55c7ebb3f85ef5b7e8a5ff1
Nirajan Dhakal <53692745+nirajandata@users.noreply.github.com>
2020-02-28 22:17:35 +0545

    Update config.php

--- contact/config.php
+++ contact/config.php
@@ -8,11 +8,16 @@ if($connec){
 else{
 	die("error is".mysqli_connect_error());
 }
-if (isset($_POST['save'])){
+if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['address'])&&isset($_POST['save'])){
+	  if (!empty($_POST['name'])) {
 	$name=$_POST['name'];
 	$address=$_POST['address'];
 	$email=$_POST['email'];
 	$connec->query("INSERT INTO ctable(name,email,address) VALUES('$name','$email','$address')") or die($connec->error);
 	$result=$connec->query("SELECT * FROM ctable") or die($connec->error);
+	}
+	else{
+		echo "<center><h1>error in the table <h1></center>";
+	}
 	}
 ?>
