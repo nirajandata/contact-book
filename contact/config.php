59400bf48c6d9e57a0910e5b0a987c3eb46ba4af
Nirajan Dhakal <53692745+nirajandata@users.noreply.github.com>
2020-03-01 19:35:07 +0545

    Add files via upload

--- contact/config.php
+++ contact/config.php
@@ -1,23 +1,58 @@
-
-<?php 
+<?php
+session_start(); 
 $connec=mysqli_connect("localhost","root","","contact");
-
-if($connec){
-	echo "<center>-------</center>";
-}
-else{
+$update=false;
+$ids=1;
+$name='';
+$address='';
+$email='';
+if(!$connec){
 	die("error is".mysqli_connect_error());
 }
-if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['address'])&&isset($_POST['save'])){
+$result=$connec->query("SELECT * FROM ctable") or die($connec->error);
+if (isset($_POST['save'])){
 	  if (!empty($_POST['name'])) {
 	$name=$_POST['name'];
 	$address=$_POST['address'];
 	$email=$_POST['email'];
 	$connec->query("INSERT INTO ctable(name,email,address) VALUES('$name','$email','$address')") or die($connec->error);
-	$result=$connec->query("SELECT * FROM ctable") or die($connec->error);
+	
+	$_SESSION['message']="Record has been saved";
+	$_SESSION['msg']="success";
+	header("location:index.php");
 	}
-	else{
-		echo "<center><h1>error in the table <h1></center>";
+	// else{
+	// 	echo "<center><h1>error in the table </center>";
+	// }
+
 	}
+	if (isset($_GET['delete'])) {
+	 $id=$_GET['delete'];
+	 $connec->query("DELETE FROM ctable WHERE id=$id") or die($connec->error);
+	 $_SESSION['message']="Record has been deleted";
+	 $_SESSION['msg']="danger";
+	 header("location:index.php");	
+	}
+	if (isset($_GET['edit'])) {
+	 $id=$_GET['edit'];
+	 
+	 $update=true;
+	 $resl=$connec->query("SELECT * FROM ctable where id=$id ") or die($connec->error);
+	 if ($id!=null) {
+	 	$row=$resl->fetch_array();
+	 	$name=$row['name'];
+	 	$address=$row['address'];
+	 	$email=$row['email'];
+	 }
+	}
+	if (isset($_POST['update'])) {
+	 $ids=$_POST['id'];
+	 $name = $_POST['name'];
+	 $email=$_POST['email'];
+	 $address=$_POST['address'];
+	 $connec->query("UPDATE ctable SET name='$name', address='$address', email='$email'  WHERE id=$ids ");
+	 $_SESSION['message']="Record has been updated";
+	 $_SESSION['msg']="warning";
+	 //header('location: index.php');
 	}
 ?>
--- contact/index.php
+++ contact/index.php
@@ -12,31 +12,46 @@
 <body>
 	<?php require_once 'config.php';
 	 ?>
+
+<?php 
+if (isset($_SESSION['message'])): ?>
+<div class="alert alert-<?=$_SESSION['msg'] ?>" role="alert">
+	<?php
+	 echo $_SESSION['message'];
+	 unset($_SESSION['message']);
+	?>
+</div>
+<?php endif ?>
 <div class="container">
 <h2 class=" blockquote text-center">A Address Book </h2>	 
 <footer class="blockquote-footer text-center">By Navaraj Dhakal</footer>
 <div class="row justify-content-center">
+	<input type="hidden" name="id" value="<?php echo $ids; ?> ">
 	<form action="" method="post">
 		<div class="form-group" >
 			<label>Name</label>
-			<input type="text" class="form-control" required="required" name="name" placeholder="Enter your name">
+			<input type="text" class="form-control" value="<?php echo $name ; ?>" required="required" name="name" placeholder="Enter your name">
 		 </div>
 		 <div class="form-group" >
 			<label>Email</label>
-			<input type="text" name="email" class="form-control" required="required" placeholder="Enter your email">
+			<input type="text" name="email" class="form-control" value="<?php echo $email ; ?>" required="required" placeholder="Enter your email">
 		 </div>
 		  <div class="form-group" >
 			<label>Address</label>
-			<input type="text" required="required" name="address" class="form-control" placeholder="Enter your address">
+			<input type="text" required="required" name="address" class="form-control" value="<?php echo $address; ?>" placeholder="Enter your address">
 		 </div>
 		 <div class="form-group">
-		 	<button type="submit" class="btn btn-primary" name="save">Save</button>
+		 	<?php if ($update == true): ?>
+		 	<button type="submit" class="btn btn-info" name="update">update</button>
+		 	<?php else: ?>
+		 		<button type="submit" class="btn btn-primary" name="save">Save</button>
+		 	<?php endif ;?>
 		 </div>
 	</form>
 </div>
 <div class="row justify-content-center">
-<table class="table align-self-center">
-<thead class="thead-light">
+<table class="table ">
+<thead >
 	<tr>
 		<th> Name</th>
 		<th> Address</th>
@@ -50,11 +65,20 @@ while($row=$result->fetch_assoc()): ?>
 	<td><?php echo $row['name']."<br>"; ?></td>
 	<td><?php echo $row['address']."<br>"; ?></td>
 	<td><?php echo $row['email']."<br>"; ?></td>	
+	<td>
+		<a href="index.php?edit=<?php echo $row['id'] ?>"
+			class="btn btn-info">Edit
+		</a>
+		<a href="config.php?delete=<?php echo $row['id'] ?>"
+			class="btn btn-danger">Trash
+		</a>
+	</td>
 	</tr>
 <?php endwhile; ?>
 
 </table>
 </div>
 </div>
+
 </body>
 </html>
